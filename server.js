a201dc3958eee3a73e2debcb8623168c294bc23e
arios10 <ariosfly3@gmail.com>
2022-02-03 00:32:39 -0700

    Set up mutations and JWT logic

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./server');
+const { typeDefs, resolvers } = require('./schemas/index.js');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,8 @@ const app = express();
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
   await server.start();
   server.applyMiddleware({ app });
